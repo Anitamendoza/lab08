1ae38a9cd268f81bbce7ba8a1cdfa4659ad0b1f5
Cristina <ana.mendoza@tecsup.edu.pe>
2023-11-03 15:19:21 -0500

    Agregar archivos de proyecto.

---
+++ lab08/lab08/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using lab08.Services;
+using lab08.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab08
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            // MainPage = new AppShell();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ lab08/lab08/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using lab08.Models;
+using lab08.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab08.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
